e9432efbec9200cd0d5e7a6fe0a5d31ad7389556
SiddareddyRajeshReddy <b230065@nitsikkkim.ac.in>
2026-03-18 16:26:39 +0530

    deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://dapper-dieffenbachia-bb2574.netlify.app/api",
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 });
